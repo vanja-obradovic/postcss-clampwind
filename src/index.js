5a14e5f4251137c7f5bd30d146c9b89750c9a3fc
danieledep <dan.depietri@gmail.com>
2025-05-18 15:40:56 +0200

    fix postcss setup

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ const plugin = require('tailwindcss/plugin')
 module.exports = plugin(function({ addVariant, theme, e, matchUtilities, matchVariant }) {
   
   console.log('Hello World')
+
+  matchUtilities(
+    {
+      'text': (value) => (
+        console.log(value),
+        {
+          fontSize: value,
+      }),
+    }
+  )
 })
